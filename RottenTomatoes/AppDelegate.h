76470fce874875bc50d8c080781cd3837ff54378
SurajSaripalli <surajsaripalli@yahoo.com>
2014-09-17 09:07:51 -0700

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Suraj Sirpilli on 9/17/14.
+//  Copyright (c) 2014 jarusElyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
